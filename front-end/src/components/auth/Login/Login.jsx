1c511bd303ceba18de5b0894cc70460c3b539612
azeddine <maslouhazeddine@gmail.com>
2023-03-16 10:04:29 +0100

    add front-end parti admin-agent

--- front-end/src/components/auth/Login/Login.jsx
+++ front-end/src/components/auth/Login/Login.jsx
@@ -1,27 +1,28 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import img from '../../images/bg.jpg'
 
 const Login = () => {
   return (
-    <main className="container">
-      <section style={{width:400}} className="mt-5 p-4 mx-auto shadow rounded-5">
-        <form>
-            <img src={require('../../images/4206917.jpg')} className='w-50 rounded-cercle' alt=""/>
-            <h2 className="text-center">Sign In</h2>
-            <div className="mb-3">
+    <main className="w-100 vh-100 d-flex justify-content-center align-items-center" style={{backgroundImage:`url(${img})`,backgroundSize:'cover'}}>
+      <section style={{maxWidth:400}} className="p-4 shadow rounded-5 bg-white">
+        <form className='text-center'>
+            <img src={require('../../images/4206917.png')} className='w-50'/>
+            <h2 className="text-center mt-2" style={{color:"#ff9847"}}>Sign <span style={{color:"#ad59e8"}}>In</span></h2>
+            <div className="mb-3 text-start">
                 <label className="form-label">Email</label>
                 <input type="email" className="form-control" placeholder="Enter your email"/>
             </div>
-            <div className="mb-3">
+            <div className="mb-3 text-start">
                 <label className="form-label">Password</label>
                 <input type="password" className="form-control" placeholder="Enter your password" />
             </div>
-            <div class="form-check mb-3">
-              <input class="form-check-input" type="checkbox" id="f"/>
-              <label class="form-check-label" for="f">Remember me</label>
+            <div className="form-check mb-3 text-start">
+              <input className="form-check-input" type="checkbox" id="f"/>
+              <label className="form-check-label" htmlFor="f">Remember me</label>
             </div>
             <div className="text-center d-grid">
-                <button type="submit" name="submit" className="btn btn-outline-dark fw-bold">Connexion</button>
+                <button type="submit" name="submit"  className="btn bg-warning-subtle">Connexion</button>
             </div>
             <hr/>
             <div className="text-center">
--- front-end/src/components/auth/Register/Register.jsx
+++ front-end/src/components/auth/Register/Register.jsx
@@ -1,8 +1,45 @@
 import React from 'react'
+import img from '../../images/bg.jpg';
 
 const Register = () => {
   return (
-    <div>Register</div>
+    <main className="w-100 vh-100 d-flex justify-content-center align-items-center" style={{backgroundImage:`url(${img})`,backgroundSize:'cover'}}>
+      <section style={{maxWidth:500}} className="p-4 shadow rounded-5 bg-white">
+        <form className='text-center'>
+            <img src={require('../../images/4206917.png')} className='w-50'/>
+            <h2 className="text-center my-4" style={{color:"#ff9847"}}>Sign <span style={{color:"#ad59e8"}}>Up</span></h2>
+            <div className="text-start d-sm-flex column-gap-3">
+                <div className='col mb-3'>
+                  <label className="form-label">name</label>
+                  <input type="text" className="form-control" placeholder="Enter your first name"/>
+                </div>
+                <div className='col mb-3'>
+                  <label className="form-label">last name</label>
+                  <input type="text" className="form-control" placeholder="Enter your last name"/>
+                </div>
+            </div>
+            <div className="mb-2 text-start">
+                <label className="form-label">Email</label>
+                <input type="email" className="form-control" placeholder="Enter your E-mail"/>
+            </div>
+            <div className="mb-2 text-start">
+                <label className="form-label">Phone</label>
+                <input type="text" className="form-control" placeholder="phone"/>
+            </div>
+            <div className="mb-2 text-start">
+                <label className="form-label">Password</label>
+                <input type="password" className="form-control" placeholder="Enter your password" />
+            </div>
+            <div className="mb-2 text-start">
+                <label className="form-label">Confirm Password</label>
+                <input type="password" className="form-control" placeholder="confirme your password" />
+            </div>
+            <div className="text-center d-grid mt-4">
+                <button type="submit" name="submit" className="btn bg-primary-subtle">Connexion</button>
+            </div>
+        </form>
+      </section>
+    </main>
   )
 }
 
