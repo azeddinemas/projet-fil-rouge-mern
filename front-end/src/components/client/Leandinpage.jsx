e17dc3b6b6581d5928ed21c422cb104eb90a9bf8
Azeddine Maslouh <12310007@moss.center>
2023-03-24 15:49:20 +0000

    add font

--- front-end/src/components/client/Leandinpage.jsx
+++ front-end/src/components/client/Leandinpage.jsx
@@ -1,6 +1,9 @@
 import React, { useState } from 'react'
 import { Outlet } from 'react-router-dom'
 import Header from './Header/Header'
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
+// import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons'
 
 const style = {
   color: "#000",
@@ -18,8 +21,9 @@ const Leandinpage = () => {
       <Header />
       <main style={darkMode ? style : style2}>
         <h5 className="form-check form-switch float-end mt-2 pe-3">
+          <label className={darkMode ? 'text-white form-check-label' : 'form-check-label'} htmlFor="kDefault"><i class="bi bi-moon-stars"></i></label>
           <input className="form-check-input" onClick={() => setdarkMode(!darkMode)} type="checkbox" role="switch" id="kDefault" />
-          <label className={darkMode ? 'text-white form-check-label' : 'form-check-label'} htmlFor="kDefault">{darkMode ? 'Light' : 'Dark'}</label>
+          <label className={darkMode ? 'text-white form-check-label' : 'form-check-label'} htmlFor="kDefault"><i class="bi bi-brightness-high"></i></label>
         </h5>
         <Outlet />
       </main>
