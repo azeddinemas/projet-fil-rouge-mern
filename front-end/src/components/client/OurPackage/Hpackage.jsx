b1dd53387141f1408f6a46055fc98354dc479012
azeddinemas <maslouhazeddine@gmail.com>
2023-04-12 15:59:01 +0000

    done auth-redux 100%

--- front-end/src/components/client/OurPackage/Hpackage.jsx
+++ front-end/src/components/client/OurPackage/Hpackage.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 const Hpackage = () => {
   return (
     <section className="py-2 text-center container">
-      <div className="row py-lg-5">
+      <div className="row pt-lg-5">
         <div className="col-lg-6 col-md-8 mx-auto">
           <h1 className="fw-light">Album des Voyages</h1>
           <p className="lead text-muted">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos non provident sunt odit ad? omnis minima saepe enim voluptatem, doloremque perferendis! Mollitia quibusdam.</p>
